36580c4d95b7c34e118a9721101360648efff80f
guillgue <github@lim.yt>
2022-05-23 12:26:49 +0200

    exercise 9.17

--- part9/patientor-frontend/src/App.tsx
+++ part9/patientor-frontend/src/App.tsx
@@ -9,6 +9,7 @@ import { Patient } from "./types";
 
 import PatientListPage from "./PatientListPage";
 import { Typography } from "@material-ui/core";
+import PatientInformation from "./components/PatientInformation";
 
 const App = () => {
   const [, dispatch] = useStateValue();
@@ -41,6 +42,7 @@ const App = () => {
           <Divider hidden />
           <Routes>
             <Route path="/" element={<PatientListPage />} />
+            <Route path="/patients/:id" element={<PatientInformation />} />
           </Routes>
         </Container>
       </Router>
---
+++ part9/patientor-frontend/src/components/PatientInformation.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { apiBaseUrl } from "./../constants";
+import { useParams } from "react-router-dom";
+import { useStateValue } from "../state";
+import axios from "axios";
+import { Patient } from "../types";
+
+const PatientInformation = () => {
+  const { id } = useParams<{ id: string }>();
+  const [{ patients }, dispatch] = useStateValue();
+
+  React.useEffect(() => {
+    const fetchPatient = async () => {
+      if (id && !patients[id].ssn) {
+        try {
+          const { data: patientFromApi } = await axios.get<Patient>(
+            `${apiBaseUrl}/patients/${id}`
+          );
+          dispatch({ type: "UPDATE_PATIENT", payload: patientFromApi });
+        } catch (e) {
+          console.error(e);
+        }
+      }
+    };
+    void fetchPatient();
+  });
+
+  if (!id) {
+    return null;
+  }
+
+  const patient = patients[id];
+
+  return (
+    <div>
+      <h4>{patient.name}</h4>
+      <div>ssn: {patient.ssn}</div>
+      <div>occupation: {patient.occupation}</div>
+    </div>
+  );
+};
+
+export default PatientInformation;
